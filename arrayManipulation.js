740fc672366d253fff6f297f87af2eb72a208a93
Ampmusicsog <aaamo-mensah@st.ug.edu.gh>
2024-05-16 19:55:47 +0000

    Task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,12 +1,31 @@
 function processArray(arr){
-    return arr.map((number) =>{
+    const processedNumbers = arr.map((number) =>{
         if(number % 2 ===0){
             return number**2;
         }
         else{
             return number *3;
         }
+        
 
     })
+    return processedNumbers;
     
 }
+
+function formatArrayString(stringArray, processedNumbers){
+    const formattedString = stringArray.map((str, index)=>{
+        const num = processedNumbers[index];
+
+        if (num % 2 ===0){
+            return str.toUpperCase();
+        }
+        else {
+            return str.toLowerCase();
+
+        }
+        
+    } )
+    return formattedString;
+}
+
